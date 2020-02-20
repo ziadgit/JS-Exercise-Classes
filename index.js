a89b2c892fc4c7bb5c1ebbfd613b2be496073e5c
Ziad <ziadbc@gmail.com>
2020-02-19 22:15:28 -0800

    pass car and lamdasian

--- index.js
+++ index.js
@@ -75,9 +75,36 @@ class Person {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
+
+
 class Car {
+  constructor(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+  }
+
+
+fill(gallons) {
+  this.tank += gallons;
+  }
+
+drive(distance) {
+  let driveable = this.milesPerGallon*this.tank;
+  if(distance<=driveable){
+  this.odometer = this.odometer + distance;
+  this.tank = this.tank - distance/this.milesPerGallon;
+}
+  else{
+    this.odometer+=driveable;
+    this.tank = 0;
+    return `I ran out of fuel at ${this.odometer} miles!`;
+  }
 
 }
+}
+
 
 /*
   TASK 3
@@ -92,8 +119,18 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
+  constructor(obj) {
+    this.name = obj.name;
+    this.age = obj.age;
+    this.location = obj.location;
+  }
 
+
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`
+  }
 }
+
 
 /*
   TASK 4
